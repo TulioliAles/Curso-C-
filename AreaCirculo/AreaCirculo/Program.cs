514557b92d22ce1a9bd10f9c40ad2a511083aa5c
TulioliAles <desmecad@hotmail.com>
2020-12-17 22:32:02 -0300

    Exercicio Aula 2 - Variáveis e Tipos

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class AreaCirculo
+    {
+        static void Main(string[] args)
+        {
+            double raioCirculo, areaCirculo;
+            const double PI = 3.1416;
+
+            raioCirculo = 10.0;
+            areaCirculo = (raioCirculo * raioCirculo) * PI;
+
+            Console.WriteLine("A área do circulo de raio 10 é: " + areaCirculo);
+        }
+    }
+}
