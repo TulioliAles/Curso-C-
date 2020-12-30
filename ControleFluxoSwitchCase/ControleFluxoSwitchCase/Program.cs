eb9535fb3729b890b203cf20ef5e4bc738164683
TulioliAles <desmecad@hotmail.com>
2020-12-29 22:05:16 -0300

    Exercicio Aula 5 - Controle Fluxo (If/Else - Switch/Case)

---
+++ ControleFluxoSwitchCase/ControleFluxoSwitchCase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ControleFluxoSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string marca;
+            string fabrica;
+
+            Console.WriteLine("Digite a marca do seu carro: ");
+            marca = Console.ReadLine();
+
+            switch (marca)
+            {
+                case "Civic":
+                case "Fit":
+                case "City":
+                    fabrica = "Honda";
+                    break;
+
+                case "Focus":
+                case "Fiesta":
+                    fabrica = "Ford";
+                    break;
+
+                case "Corolla":
+                    fabrica = "Toyota";
+                    break;
+
+                default:
+                    fabrica = "Desconhecida";
+                    break;
+            }
+
+            Console.WriteLine("A fabricante do seu carro é {0}. ", fabrica);
+        }
+    }
+}
