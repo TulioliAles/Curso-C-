d176fac76e1ec9aead346d4c9bcadebd69f9eb9f
TulioliAles <desmecad@hotmail.com>
2020-12-29 21:53:13 -0300

    Exercicio Aula 5 - Controle Fluxo (If/Else - Switch/Case)

---
+++ ControleFluxoIfElse/ControleFluxoIfElse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ControleFluxoIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um número de 0 a 10: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > 10)
+            {
+                Console.WriteLine("O número tem que estar entre 0 e 10!");
+            } else
+            {
+                if (numero < 0)
+                {
+                    Console.WriteLine("O número deve ser maior que 0!");
+                } else
+                {
+                    Console.WriteLine("O número digitado foi: " + numero);
+                }
+            }
+        }
+    }
+}
