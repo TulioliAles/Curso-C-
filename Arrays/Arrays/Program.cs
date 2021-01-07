cc2b5dba71aa7aa18679a4a8ee6e14b8218eebac
TulioliAles <desmecad@hotmail.com>
2021-01-06 22:01:10 -0300

    Exercicio Aula 12 - Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vogais = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int i = 0; i <= vogais.Length - 1; i++)
+            {
+                Console.WriteLine(vogais[i]);
+            }
+
+
+            int[] numeros = new int[30];
+
+            for(int j = 0; j <= numeros.Length - 1; j++)
+            {
+                numeros[j] = j;
+            }
+
+            for (int j = 0; j <= numeros.Length - 1; j++)
+            {
+                Console.WriteLine("O número no indice [" + j + "] = " + numeros[j]);
+            }
+        }
+    }
+}
