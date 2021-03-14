69a96e07d87e6f79dbf9c2a4f499fdfeaad9fbd8
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-03-14 11:06:29 -0300

    Commit

---
+++ ColeçõesComGenerics/ColeçõesComGenerics/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeçõesComGenerics
+{
+    public class Carro
+    {
+        public int ID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+    }
+}
---
+++ ColeçõesComGenerics/ColeçõesComGenerics/ColecoesGenerics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ColeçõesComGenerics
+{
+    class ColecoesGenerics
+    {
+        static void Main(string[] args)
+        {
+            List<Carro> carros = new List<Carro>()
+            {
+                new Carro {ID = 1, Marca = "VW", Modelo = "Fusca"},
+                new Carro {ID = 2, Marca = "VW", Modelo = "Kombi"},
+                new Carro {ID = 3, Marca = "Chevrolet", Modelo = "Opala"},
+                new Carro {ID = 4, Marca = "Ford", Modelo = "Corcel"}
+            };
+
+            foreach (var carro in carros)
+            {
+                Console.WriteLine(string.Format("ID: {0} - Marca: {1} - Modelo: {2}", carro.ID, carro.Marca, carro.Modelo));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
