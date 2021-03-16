28200196a755633cb85e8112768db7cb3abc8d3d
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-03-15 21:53:55 -0300

    Exercicio - Listagem com Action

---
+++ Action/Action/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Action
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Usuario> usuarios = new List<Usuario>() {
+                new Usuario() { ID = 1, nome = "Pedro"},
+                new Usuario() { ID = 2, nome = "Amanda"},
+                new Usuario() { ID = 3, nome = "Gabriel"},
+                new Usuario() { ID = 4, nome = "Jussara"}
+            };
+
+            usuarios.ForEach(new Action<Usuario>(MostrarDados));
+
+            Console.ReadLine();
+        }
+
+        private static void MostrarDados (Usuario user)
+        {
+            Console.WriteLine(user.ID + "-" + user.nome);
+        }
+    }
+}
---
+++ Action/Action/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Action
+{
+    public class Usuario
+    {
+        public int ID { get; internal set; }
+
+        public string nome { get; set; }
+
+    }
+}
